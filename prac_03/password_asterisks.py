2eb6dbb8d633125a3681e53ee533b0fef1b31d9f
qtntom <qtntom@yahoo.com>
2019-03-28 14:06:10 +1000

    add password check program

---
+++ prac_03/password_asterisks.py
@@ -0,0 +1,12 @@
+MIN_LEN = 5
+
+pw = input('what is ur password?')
+while len(pw) < MIN_LEN:
+    print('Invalid password!')
+    pw = input('what is ur password?')
+
+masked_pw = ''
+for i in range(len(pw)):
+    masked_pw += '*'
+
+print(masked_pw)
