c73eb907ca1db6821fd27ee294837f40363454f3
qtntom <qtntom@yahoo.com>
2019-03-28 14:13:20 +1000

    refactor password program to use functions

--- prac_03/password_asterisks.py
+++ prac_03/password_asterisks.py
@@ -1,12 +1,24 @@
-MIN_LEN = 5
+def main():
+    MIN_LENGH = 5
 
-pw = input('what is ur password?')
-while len(pw) < MIN_LEN:
-    print('Invalid password!')
+    pw = get_password(MIN_LENGH)
+
+    print_asterisks(pw)
+
+
+def print_asterisks(pw):
+    masked_pw = ''
+    for i in range(len(pw)):
+        masked_pw += '*'
+    print(masked_pw)
+
+
+def get_password(MIN_LEN):
     pw = input('what is ur password?')
+    while len(pw) < MIN_LEN:
+        print('Invalid password!')
+        pw = input('what is ur password?')
+    return pw
 
-masked_pw = ''
-for i in range(len(pw)):
-    masked_pw += '*'
 
-print(masked_pw)
+main()
