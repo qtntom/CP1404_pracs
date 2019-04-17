1010ca941fa0b6c5b476cea337a9207a6b045655
qtntom <qtntom@yahoo.com>
2019-04-17 14:15:07 +1000

    complete walkthrough, add functions in car and used_cars

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -6,14 +6,22 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
-    my_car.drive(30)
-    print("fuel =", my_car.fuel)
-    print("odo =", my_car.odometer)
-    print(my_car)
+    # my_car = Car(180)
+    # my_car.drive(30)
+    # print("fuel =", my_car.fuel)
+    # print("odo =", my_car.odometer)
+    # print(my_car)
+    #
+    # print("Car {}, {}".format(my_car.fuel, my_car.odometer))
+    # print("Car {self.fuel}, {self.odometer}".format(self=my_car))
 
-    print("Car {}, {}".format(my_car.fuel, my_car.odometer))
-    print("Car {self.fuel}, {self.odometer}".format(self=my_car))
+    limo = Car('Limosine', 100)
+    limo.add_fuel(20)
+    print('limo fuel = {}'.format(limo.fuel))
+    limo.drive(115)
+    print('limo odo = {self.odometer}'.format(self=limo))
+    print(limo)
+    print(limo.name)
 
 
 main()
