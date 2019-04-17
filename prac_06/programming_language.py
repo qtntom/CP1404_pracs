e3f4b948058e0268ed412f08cb0538145e59fd3f
qtntom <qtntom@yahoo.com>
2019-04-18 08:55:51 +1000

    finish intermediate exercise - programming language class

---
+++ prac_06/programming_language.py
@@ -0,0 +1,18 @@
+'''
+Programming language class
+'''
+
+
+class ProgrammingLanguage:
+    def __init__(self, name='', typing='Static', reflection=True, year=1990):
+        self.name = name
+        self.typing = typing
+        self.reflection = True
+        self.year = year
+
+    def is_dynamic(self):
+        return self.typing == 'Dynamic'
+
+    def __str__(self):
+        return '{}, {} Typing, Reflection={}, First appeared in {}'.format(self.name, self.typing, self.reflection,
+                                                                           self.year)
