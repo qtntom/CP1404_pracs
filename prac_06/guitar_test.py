333ad648df5c9a2f2fa83a7f2de4f8969d2d0084
qtntom <qtntom@yahoo.com>
2019-04-18 10:26:26 +1000

    playing guitar

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,14 @@
+'''
+Test code for guitar class
+'''
+
+from prac_06.guitar import Guitar
+
+guitar_1 = Guitar('Yamaha', 1920, 20000.542)
+guitar_2 = Guitar('Honda', 1990, 2043.5)
+
+print('{:8} get_age() - Expected 99. Got {}'.format(guitar_1.name, guitar_1.get_age()))
+print('{:8} get_age() - Expected 29. Got {}'.format(guitar_2.name, guitar_2.get_age()))
+
+print('{:8} is_vintage() - Expected True. Got {}'.format(guitar_1.name, guitar_1.is_vintage()))
+print('{:8} is_vintage() - Expected False. Got {}'.format(guitar_2.name, guitar_2.is_vintage()))
---
+++ prac_06/guitars.py
@@ -0,0 +1,24 @@
+'''
+Client code, playing with the guitars...class
+'''
+
+from prac_06.guitar import Guitar
+
+guitars = []
+guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+print('My guitars!')
+
+name = input('Name: ')
+while name != '':
+    year = int(input('Year: '))
+    cost = float(input('Cost: '))
+    guitars.append(Guitar(name, year, cost))
+    print('{} ({}) : ${:,.2f} added'.format(name, year, cost))
+    name = input('Name: ')
+
+print('These are my guitars:')
+for i, guitar in enumerate(guitars):
+    vintage_string = '(vintage)' if guitar.is_vintage() else ''
+    print("Guitar {}: {:>15} ({}), worth ${:10,.2f} {}".format(i + 1, guitar.name, guitar.year, guitar.cost,
+                                                              vintage_string))
