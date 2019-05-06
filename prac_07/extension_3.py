99f6be3026a70df3051dc5b33bcbc0533bd8776c
qtntom <qtntom@yahoo.com>
2019-05-06 18:36:18 +1000

    finalise extension 3

--- prac_07/extension_3.py
+++ prac_07/extension_3.py
@@ -6,13 +6,31 @@ By Q.T
 
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.app import StringProperty
 
 
 class TemperatureConverter(App):
+    fahra = StringProperty()
+    celcius = StringProperty()
+
     def build(self):
         self.title = 'Temperature conversion'
         self.root = Builder.load_file('extension_3.kv')
         return self.root
 
-    def convert_to_fahra(self, celcius):
-        return celcius * 9 / 5 + 32
+    def convert_to_fahra(self, celcius_text):
+        try:
+            self.fahra = str((float(celcius_text) * 9 / 5) + 32)
+        except ValueError:
+            self.celcius = '0'
+            self.fahra = ''
+
+    def convert_to_celcius(self):
+        try:
+            fahra = float(self.root.ids.fahra_value.text)
+            self.celcius = str((fahra - 32) * 5 / 9)
+        except ValueError:
+            self.celcius = '0'
+            self.fahra = ''
+
+TemperatureConverter().run()
