0145f049afd91e1cc81c0682d1e9eb46f3acb71e
qtntom <qtntom@yahoo.com>
2019-05-19 17:09:09 +1000

    add docstrings and comments, complete

--- prac_09/sort_files_1.py
+++ prac_09/sort_files_1.py
@@ -1,13 +1,20 @@
+"""
+create folders for each extension type and move files into
+By Q.T
+"""
+
 import os
 import shutil
 
 
 def main():
+    """Main function"""
     os.chdir('FilesToSort')
     print('Current directory: {}'.format(os.getcwd()))
 
     extensions = []
     filenames = os.listdir('.')
+    # Loop through every filename, split the extension from the name, and check if extension already been found
     for filename in filenames:
         extension = os.path.splitext(filename)[1]
         if extension not in extensions:
